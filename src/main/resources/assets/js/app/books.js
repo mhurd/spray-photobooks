921918842e2d9918ea314e233993006a36f0038a
mhurd <mjwhurd@gmail.com>
2014-08-29 13:43:19 +0100

    Addition of some helper functions for future use

--- src/main/resources/assets/js/app/books.js
+++ src/main/resources/assets/js/app/books.js
@@ -1,14 +1,49 @@
 (function ($) {
 
+    function createImage(bookJson) {
+        if (bookJson.smallBookCover != null) {
+            return "<a class='thumbnail'><img src='" + bookJson.smallBookCover + "'/></a>";
+        } else {
+            return "<a class='thumbnail'><img src='../../../assets/img/image-not-found.jpg'/></a>";
+        }
+    }
+
+    function getDetailDListItem(bookJson, attribute) {
+        if (bookJson[attribute] != null) {
+            return "<dt>" + attribute + ":</dt><dd>" + bookJson[attribute] + "</dd>";
+        } else {
+            return "<dt>" + attribute + ":</dt><dd>not set</dd>";
+        }
+    }
+
+    function createBookDetailPanel(bookJson) {
+        var details = [];
+        details.push(getDetailDListItem(bookJson, "title"));
+        details.push(getDetailDListItem(bookJson, "authors"));
+        details.push(getDetailDListItem(bookJson, "isbn"));
+        var dl = $('<dl class="dl-horizontal">');
+        details.map(function(item) {
+            dl.append(item);
+        });
+        var tdd = $('<td align="left">');
+        tdd.append(dl);
+        var tdi = $('<td align="right">');
+        tdi.append(createImage(bookJson));
+        var tr = $('<tr>');
+        tr.append(tdi);
+        tr.append(tdd);
+        var table = $('<table class="bookDetailPanel">');
+        table.append(tr);
+        return table;
+    }
+
     function populate() {
         var hostname = location.host;
         $.getJSON("http://" + hostname + "/books", function (data) {
             $.each(data, function (key, val) {
-                if (val.smallBookCover != null) {
-                    $('#items').append("<li class='span2'><a class='thumbnail'><img src='" + val.smallBookCover + "'/></a></li>");
-                } else {
-                    $('#items').append("<li class='span2'><a class='thumbnail'><img src='../../../assets/img/image-not-found.jpg'/></a></li>");
-                }
+                var item = $('<li>');
+                item.append(createImage(val));
+                $('#items').append(item);
             });
         });
     };
